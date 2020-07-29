07f3aa3128bbcfa8372a62c738a0787b5bfff168
Helma van der Linden <hepabolu@gmail.com>
2020-07-29 16:09:41 +0200

    grab the mp3 file links from the md files, because they are modified in the book

--- scripts/grep_mp3.sh
+++ scripts/grep_mp3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # grep the urls from the files
-grep media.blubrry book/*.adoc | cut -d: -f2- | cut -d= -f3- | grep -v 'controls' | cut -d+ -f1 | cut -d\" -f2 | grep -v 'url-mp3'
+grep blubrry convert2/*.md | cut -d= -f2 | grep -v 'data-external' | grep -v 'src' | cut -d'?' -f1 | cut -d'"' -f2 | sort |uniq
